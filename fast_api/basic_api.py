37593e379f39886416c679eafe350a352bdd15f0
Masoom Tariq <167184654+masoomtariq@users.noreply.github.com>
2025-06-21 12:17:09 +0000

    up

--- fast_api/basic_api.py
+++ fast_api/basic_api.py
@@ -1,4 +1,11 @@
 from fastapi import FastAPI
+import uvicorn
 
-app = FastAPI(title="Basic api", description="THis api is mainly build for testing")
+app = FastAPI(title="Basic api", description="THis api is mainly build for testing", version = '1.0.0')
 
+@app.get('/')
+def main():
+    return {'message': "this is my first api"}
+
+if __name__ == "__main__":
+    uvicorn.run("basic_api:app", reload=True)
